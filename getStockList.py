610d62f2ffab2ee46976feadf6c3dfd426ef6500
StrongX <strongxlx@gmail.com>
2019-03-14 18:05:19 +0800

    [update]添加了股票列表查询，并插入mongodb

--- getStockList.py
+++ getStockList.py
@@ -1,4 +1,8 @@
 import tushare as ts
+import pymongo
+client = pymongo.MongoClient(host='localhost', port=27017)
+db = client.ffgHarvester
+col = db.stockList
 
 ts.set_token('495bd6a4d40acef11e6a222a1632889b27c60938aa9decba468c472b')
 
@@ -7,4 +11,7 @@ pro = ts.pro_api()
 data = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,fullname,enname,market,exchange,curr_type,list_status,list_date,delist_date,is_hs')
 
 for index,row in data.iterrows():
-	print(dict(row))
+	obj = dict(row)
+	obj['_id'] = obj['ts_code']
+	result = col.update({"_id":obj['_id']},{"$set":obj},upsert=True)
+	print(result)
