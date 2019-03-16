f2e7b40ef57c7ec955d5a09eabd523b0e6878392
StrongX <strongxlx@gmail.com>
2019-03-16 21:30:16 +0800

    [update]修改为一次查询，group 统计  优化了统计速度

--- riseCalculate.py
+++ riseCalculate.py
@@ -21,30 +21,25 @@ def riseCalculate(ascending):
 	firstDayCal = dayList[0]
 	firstDayStr = firstDayCal['cal_date'] #最后一个交易日的日期
 	stockList = list(stockListCol.find())
+	stockDf = []
+	for stock in stockList:
+		stockDf.append(pd.Series(stock))
+	stockDf = pd.DataFrame(stockDf)
 	count = 0
 	calculatedList = []
-	for stock in stockList:
-		ts_code = stock['ts_code']
-		stockDailys = list(stockDailyCol.find({'ts_code':ts_code,'trade_date':{'$lte':firstDayStr},'trade_date':{'$gte':lastDayStr}}))
-		pct_chg = 0.0
-		for daily in stockDailys:
-			pct_chg+=daily['pct_chg']
-		stock['pct_chg'] = pct_chg
-		calculatedList.append(pd.Series(stock))
-		print('股票名称：'+stock['name']+'  股票代码：'+stock['ts_code']+' '+str(dayCount)+'日累计涨幅：'+'%.2f'%stock['pct_chg']+'%')
-		count+=1
-		# if count == 10:
-		# 	break      #跑的太鸡儿慢了  先跑10支股票  后面优化速度
-
-	df = pd.DataFrame(calculatedList).sort_values(by="pct_chg",ascending=ascending)  #根据涨幅排序
-	df = df.reset_index(drop=True)   #重建索引
+	stockDailys = list(stockDailyCol.find({'trade_date':{'$lte':firstDayStr},'trade_date':{'$gte':lastDayStr}}))
+	for daily in stockDailys:
+		calculatedList.append(pd.Series(daily))
+	calculateDf = pd.DataFrame(calculatedList).groupby('ts_code')[['pct_chg']].sum()
+	res = pd.merge(calculateDf, stockDf, on='ts_code').sort_values(by="pct_chg",ascending=ascending)  #根据涨幅排序
+	res = res.reset_index(drop=True)   #重建索引
 	while True:
 		count = input("look for top count: ")
-		print(df.loc[0:int(count)-1,['name','ts_code','pct_chg']])
+		print(res.loc[0:int(count)-1,['name','ts_code','pct_chg']])
 
 def main():
 	print("1:连续涨幅统计\n2:连续跌幅统计")
-	select = input("Enter your input: ")
+	select = input("Enter your input: ") 
 	if select == '1':
 		riseCalculate(False)
 	elif select == '2':
