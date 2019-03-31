2d3b33f91f38a7d1c7a674fc5019390f97309055
StrongX <strongxlx@gmail.com>
2019-03-31 23:47:52 +0800

    [fix]修复了计算匀线脚本的bug

--- calculateAverage.py
+++ calculateAverage.py
@@ -5,7 +5,7 @@ import pymongo
 import pandas as pd
 import time
 import multiprocessing
-
+from bson.objectid import ObjectId
 
 client = pymongo.MongoClient(host='localhost', port=27017)
 db = client.ffgHarvester
@@ -33,12 +33,12 @@ def calculateWithDays(days):
 def calculateWithProcess(days,stocks):
 	key = 'ma'+str(days)
 	for index,daily in stocks.iterrows():	#2、循环每一个日线计算匀线
-		stockDf = allStockDf[(allStockDf['ts_code']==daily['ts_code']) & (allStockDf['trade_date']<=daily['trade_date'])].sort_values(by="trade_date",ascending=False).reset_index(drop=True).loc[0:days]
+		stockDf = allStockDf[(allStockDf['ts_code']==daily['ts_code']) & (allStockDf['trade_date']<=daily['trade_date'])].sort_values(by="trade_date",ascending=False).reset_index(drop=True).iloc[0:days]
 		#3、找出此日线前n天的所有日线
 		# if len(maStocks) < days:
 		# 	continue  #若数据库数据不够匀线统计则不做统计
 		value = stockDf['close'].mean()	#4、计算收盘价均价
-		stockDailyCol.update({"_id":daily['_id'],},{"$set":{key:value}}) 	#5、插入计算号的均价。  字段名，如：ma20
+		stockDailyCol.update({"_id":ObjectId(daily['_id']),},{"$set":{key:value}}) 	#5、插入计算号的均价。  字段名，如：ma20
 		print('股票代码：'+daily['ts_code']+'  交易日期：'+daily['trade_date']+'   '+str(days)+'日均价:'+str(value))
 
 def main():
